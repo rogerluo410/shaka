7799582775e8c9d343881793123169d60e195f33
rogerluo410 <rogerluo410@gmail.com>
2024-10-17 17:10:06 +0800

    commit 4
    commit 2

--- app/javascript/packs/vue/clue.vue
+++ app/javascript/packs/vue/clue.vue
@@ -3,7 +3,11 @@
     <p class="title" style="font-size: 20px">test clue</p>
     <p class="title" style="font-size: 20px">commit 1</p>
     <p class="title" style="font-size: 20px">commit 2</p>
+<<<<<<< HEAD
     <p class="title" style="font-size: 20px">commit 3</p>
+    <p class="title" style="font-size: 20px">commit 4</p>
+=======
+>>>>>>> 5aa3f00 (commit 2)
   </div>
 </template>
 

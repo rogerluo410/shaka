607267dccc7773c1eda5c3cdde2270f48d4238de
rogerluo410 <rogerluo410@gmail.com>
2024-10-17 17:09:48 +0800

    commit 3

--- app/javascript/packs/vue/clue.vue
+++ app/javascript/packs/vue/clue.vue
@@ -3,6 +3,7 @@
     <p class="title" style="font-size: 20px">test clue</p>
     <p class="title" style="font-size: 20px">commit 1</p>
     <p class="title" style="font-size: 20px">commit 2</p>
+    <p class="title" style="font-size: 20px">commit 3</p>
   </div>
 </template>
 

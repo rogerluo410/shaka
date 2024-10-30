af3d6cddfd157fab06d1849b6cd9f48cf359f8b7
rogerluo410 <rogerluo410@gmail.com>
2024-10-30 23:13:29 +0800

    Test vue draggable in compatible vue3

--- app/javascript/packs/vue/clue.vue
+++ app/javascript/packs/vue/clue.vue
@@ -9,15 +9,76 @@
     <p>
       <a href="/tricks">Go tricks</a>
     </p>
+
+    <div class="col-6">
+      <h3>Draggable {{ draggingInfo }}</h3>
+
+      <draggable
+        :list="list"
+        :disabled="!enabled"
+        item-key="name"
+        class="list-group"
+        ghost-class="ghost"
+        :move="checkMove"
+        @start="dragging = true"
+        @end="dragging = false"
+      >
+        <template #item="{ element }">
+          <div class="list-group-item" :class="{ 'not-draggable': !enabled }">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import draggable from "vuedraggable";
+draggable.compatConfig = { MODE: 3 };
+
+export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      enabled: true,
+      list: [
+        { name: "John", id: 0 },
+        { name: "Joao", id: 1 },
+        { name: "Jean", id: 2 },
+      ],
+      dragging: false,
+    };
+  },
+  computed: {
+    draggingInfo() {
+      return this.dragging ? "under drag" : "";
+    },
+  },
+  methods: {
+    checkMove: function (e) {
+      window.console.log("Future index: " + e.draggedContext.futureIndex);
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
 .title {
   color: red !important;
+}
+.buttons {
+  margin-top: 35px;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.not-draggable {
+  cursor: no-drop;
 }
 </style>
